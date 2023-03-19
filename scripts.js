3cfcaffefa48677ec15b2af81136bd533b8351ee
vmantri83 <vmantri83@gmail.com>
2023-03-19 12:34:21 +0530

    made changes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+let date = document.getElementById("date");
+// creating the date object and getting the date and time
+let newDate = new Date();
+let year = newDate.getFullYear();
+let month = newDate.getMonth();
+let todaySDate = newDate.getDate();
+date.innerHTML = " " + todaySDate + "." + month + "." + year;
+time.innerHTML = hours + ": " + minutes + ": " + seconds;
+
 document.querySelector("#add").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
     alert("Please enter a Task");
@@ -6,12 +15,12 @@ document.querySelector("#add").onclick = function () {
 <div class="task">
     <label class="taskCheck">
         <input type="checkbox" class="taskCheckbox">
-        <span> id="taskname">
+        <span id="taskname">
             ${document.querySelector("#newtask input").value}
         </span>
     </label>
     <button class="delete">
-        <i> class="far fa-trash-alt"></i>
+        <i class="far fa-trash-alt"></i>
     </button>
 </div>
 `;
