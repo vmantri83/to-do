31d2cfeba71bfe46f531870be1d72cb273689199
vmantri83 <vmantri83@gmail.com>
2023-09-03 11:56:22 +0530

    made changes

--- scripts.js
+++ scripts.js
@@ -1,34 +1,90 @@
-let date = document.getElementById("date");
-// creating the date object and getting the date and time
-let newDate = new Date();
-let year = newDate.getFullYear();
-let month = newDate.getMonth();
-let todaySDate = newDate.getDate();
-date.innerHTML = " " + todaySDate + "." + month + "." + year;
-time.innerHTML = hours + ": " + minutes + ": " + seconds;
+// app.js
+window.onload = () => {
+  const form1 = document.querySelector("#addForm");
 
-document.querySelector("#add").onclick = function () {
-  if (document.querySelector("#newtask input").value.length == 0) {
-    alert("Please enter a Task");
-  } else {
-    document.querySelector("#tasks").innerHTML += `
-<div class="task">
-    <label class="taskCheck">
-        <input type="checkbox" class="taskCheckbox">
-        <span id="taskname">
-            ${document.querySelector("#newtask input").value}
-        </span>
-    </label>
-    <button class="delete">
-        <i class="far fa-trash-alt"></i>
-    </button>
-</div>
-`;
-    var current_tasks = document.querySelectorAll(".delete");
-    for (var i = 0; i < current_tasks.length; i++) {
-      current_tasks[i].onclick = function () {
-        this.parentNode.remove();
-      };
+  let items = document.getElementById("items");
+  let submit = document.getElementById("submit");
+
+  let editItem = null;
+
+  form1.addEventListener("submit", addItem);
+  items.addEventListener("click", removeItem);
+};
+
+function addItem(e) {
+  e.preventDefault();
+
+  if (submit.value != "Submit") {
+    console.log("Hello");
+
+    editItem.target.parentNode.childNodes[0].data =
+      document.getElementById("item").value;
+
+    submit.value = "Submit";
+    document.getElementById("item").value = "";
+
+    document.getElementById("lblsuccess").innerHTML =
+      "Text edited successfully";
+
+    document.getElementById("lblsuccess").style.display = "block";
+
+    setTimeout(function () {
+      document.getElementById("lblsuccess").style.display = "none";
+    }, 3000);
+
+    return false;
+  }
+
+  let newItem = document.getElementById("item").value;
+  if (newItem.trim() == "" || newItem.trim() == null) return false;
+  else document.getElementById("item").value = "";
+
+  let li = document.createElement("li");
+  li.className = "list-group-item";
+
+  let deleteButton = document.createElement("button");
+
+  deleteButton.className = "btn-danger btn btn-sm float-right delete";
+
+  deleteButton.appendChild(document.createTextNode("Delete"));
+
+  let editButton = document.createElement("button");
+
+  editButton.className = "btn-success btn btn-sm float-right edit";
+
+  editButton.appendChild(document.createTextNode("Edit"));
+
+  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(deleteButton);
+  li.appendChild(editButton);
+
+  items.appendChild(li);
+}
+
+function removeItem(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you Sure?")) {
+      let li = e.target.parentNode;
+      items.removeChild(li);
+      document.getElementById("lblsuccess").innerHTML =
+        "Text deleted successfully";
+
+      document.getElementById("lblsuccess").style.display = "block";
+
+      setTimeout(function () {
+        document.getElementById("lblsuccess").style.display = "none";
+      }, 3000);
     }
   }
-};
+  if (e.target.classList.contains("edit")) {
+    document.getElementById("item").value =
+      e.target.parentNode.childNodes[0].data;
+    submit.value = "EDIT";
+    editItem = e;
+  }
+}
+
+function toggleButton(ref, btnID) {
+  document.getElementById(btnID).disabled = false;
+}
